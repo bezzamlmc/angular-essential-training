300a90e749f0fab84fa15dad9886dfc3f79fd8cd
Laura B <lmcarcione@gmail.com>
2020-08-06 17:07:44 -0700

    Fix bad merge

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,4 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
-<<<<<<< HEAD
 platformBrowserDynamic().bootstrapModule(AppModule);
-=======
-platformBrowserDynamic().bootstrapModule(AppModule);
->>>>>>> 02_01b_LMC
