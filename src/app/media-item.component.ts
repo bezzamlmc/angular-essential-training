8ee20db504ec7fdb52a84b553b986cbef040fc55
Laura B <lmcarcione@gmail.com>
2020-08-06 18:21:57 -0700

    Interpolation practice

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
 
+// NOTE from LMC: nane can be used in the html document
+// Same with the wasWatched method
 @Component({
   selector: 'mw-media-item',
   templateUrl: './media-item.component.html',
   styleUrls: ['./media-item.component.css']
 })
-export class MediaItemComponent {}
+export class MediaItemComponent {
+  name = 'The Redemption';
+  wasWatched() {
+    return true;
+  }
+}
